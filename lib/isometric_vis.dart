fcb26e441641674439c713fe8094f641800372dc
James Porter <james@amimetic.co.uk>
2019-03-16 20:33:17 +0000

    something working... let's tidy up.... maybe add slider for scale(?) and submit

--- lib/isometric_vis.dart
+++ lib/isometric_vis.dart
@@ -3,48 +3,48 @@ import 'isometric_calc.dart';
 import 'dart:math';
 
 class IsoVisPainter extends CustomPainter {
+  double dt;
+
+  IsoVisPainter(this.dt);
+
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint();
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
 
-    paint.color = Colors.white;
-    var rect = Rect.fromLTWH(0, 0, size.width, size.height);
-    canvas.drawRect(rect, paint);
+    final bgPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8
+      ..color = Colors.white;
 
-    var center = Offset(size.width / 2, size.height / 2 + 5);
+    for (int j = 9; j >= 0; j--) {
+      final linePath = Path();
 
-    paint.color = Colors.grey;
+      paint.color = Color.fromARGB(255, 41, 204 - j * 12, 130 + j * 4);
 
-    canvas.drawCircle(center, 75.0, paint);
+      for (int i = 0; i < 25; i++) {
+        final xy = Point3D(
+                i.toDouble() * 4,
+                (2 + 18 * dt) * cos(31.4 * dt + i / (2 + j / 5)),
+                j.toDouble() * 10.0)
+            .intoIso2D(2)
+            .offsetted(size.width / 2, size.height * 0.5 + 100);
 
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 2;
-    paint.color = Colors.lightBlue;
+        if (i == 0) {
+          linePath.moveTo(xy.x, xy.y);
+        } else {
+          linePath.lineTo(xy.x, xy.y);
+        }
+      }
 
-    canvas.drawPath(
-        Path()
-          ..moveTo(100, 100)
-          ..lineTo(200, 100)
-          ..lineTo(200, 200)
-          ..close(),
-        paint);
-
-    canvas.drawCircle(center, 75.0, paint);
-
-    final linePath = Path()..moveTo(100, 100);
-
-    for (int i = 0; i < 100; i++) {
-      linePath.lineTo(100.0 + i * 4, 100 + 20 * cos(i / 4));
+      canvas.drawPath(linePath, bgPaint);
+      canvas.drawPath(linePath, paint);
     }
-    linePath.close();
-    canvas.drawPath(linePath, paint);
   }
 
-  void drawCube(Canvas canvas, Point3D minP, Point3D maxP) {}
-
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return null;
+  bool shouldRepaint(IsoVisPainter oldDelegate) {
+    return true;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,64 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: IsoPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class IsoPage extends StatelessWidget {
+class IsoPage extends StatefulWidget {
+  @override
+  _IsoPageState createState() => _IsoPageState();
+}
+
+class _IsoPageState extends State<IsoPage> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  bool playing = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5))
+          ..repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Isometrics"),
-        ),
-        body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: CustomPaint(painter: IsoVisPainter(), child: Container())));
+      appBar: AppBar(
+        title: Text("Isometrics"),
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) {
+                return CustomPaint(
+                    painter: IsoVisPainter(controller.value),
+                    child: Container());
+              })),
+      floatingActionButton: playing
+          ? FloatingActionButton(
+              onPressed: () {
+                setState(() => this.playing = false);
+                controller.stop();
+              },
+              child: Icon(Icons.pause),
+            )
+          : FloatingActionButton(
+              onPressed: () {
+                setState(() => this.playing = true);
+                controller.repeat();
+              },
+              child: Icon(Icons.play_arrow),
+            ),
+    );
   }
 }
